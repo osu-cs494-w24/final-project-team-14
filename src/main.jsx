e2d51624dea9834628558a5c469a162d3c0323d8
gregorynewman12 <gregorynewman12@gmail.com>
2024-03-12 17:11:23 -0700

    Header is working

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Root from './pages/Root'
 import Home from './pages/HomePage'
+import App from './App'
 import './index.css'
 //import store from './redux/store'
 
@@ -13,10 +14,10 @@ const queryClient = new QueryClient()
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Root />,
-        children: [
+        element: <App />,
+        /* children: [
             { path: "", element: <Home />},
-        ]
+        ] */
     }
 ])
 
@@ -24,7 +25,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
             {/*<Provider store={store}>*/}
-                <RouterProvider router={router} />
+            <RouterProvider router={router} />
             {/*</Provider>*/}
         </QueryClientProvider>
     </React.StrictMode>

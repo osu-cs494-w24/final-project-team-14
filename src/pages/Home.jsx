05844de9b2ca676860d7428f1fc0789f36d4cbf9
Cody DePeel <codydepeel@gmail.com>
2024-03-16 13:21:31 -0700

    added styling to search bar on main page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
+import { css } from '@emotion/react'
 import styled from '@emotion/styled'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 const MapContainer = styled.div`
     text-align: center;
@@ -9,6 +12,41 @@ const MapContainer = styled.div`
         height: 3vh;
         border-radius: 20px;
         text-align: center;
+    }
+`
+
+const SearchBarContainer = styled.div`
+    position: relative;
+    width: 40vw;
+    margin: auto; // Centers the search bar
+
+    // Animations for search bar styling
+    transition: transform 0.25s ease-in-out;
+    &:hover {
+        transform: scale(1.08);
+    }
+`
+
+const MagnifyingGlassIcon = styled(FontAwesomeIcon)`
+    // CSS to shift the icon inside of the search bar
+    
+    position: absolute;
+    left: 10px;
+    top: 50%;
+    transform: translateY(-90%);
+`
+
+const SearchBarInput = styled.input`
+    width: 100%;
+    height: 3vh;
+    border-radius: 20px;
+    text-align: center;
+
+    &:focus {
+        border-color: #007bff;
+        
+        // Sourced from "https://getcssscan.com/css-box-shadow-examples", example #5.
+        box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
     }
 `
 
@@ -23,7 +61,18 @@ export default function Home() {
                 console.log("Place query: ",text)
                 setQuery(text)
             }}>
-                <input value={text} onChange={(e) => setText(e.target.value)} />
+                <SearchBarContainer>
+
+                    <MagnifyingGlassIcon icon={faMagnifyingGlass}/>
+                    <SearchBarInput
+                        value={text} 
+                        placeholder={`Enter a city or zipcode...`}
+                        onChange={(e) => setText(e.target.value)}>
+                    </SearchBarInput>
+
+                </SearchBarContainer>
+            
+            
             </form>
             <iframe
                 width="100%"

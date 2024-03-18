fd29cedaf9fc2820d6dd276c88a44834ac0b4de5
Dominick Tolomeo <tolomeodom@gmail.com>
2024-03-17 21:21:01 -0700

    added event searching

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -10,18 +10,16 @@ export default function ResultCard(props) {
                 <div className="result-title-container">
 
                     <NavLink to={`/events/${props.id}`}>
-                        {console.log("== props.id: ", props.id)}
-                        
                         <h2 className="result-title">{props.name}</h2>
                     </NavLink>
 
                 </div>
                 <div className="result-details-container">
-                    <img src="/public/map-pin.png" alt="pin" />
+                    <img src="/map-pin.png" alt="pin" />
                     <h3 className="result-location">{props.location}</h3>
-                    <img src="/public/calendar.png" alt="calendar" />
+                    <img src="/calendar.png" alt="calendar" />
                     <h3 className="result-location">{props.date}</h3>
-                    <img src="/public/clock.webp" alt="clock" />
+                    <img src="/clock.webp" alt="clock" />
                     <h3 className="result-time">{props.time}</h3>
                 </div>
             </div>
